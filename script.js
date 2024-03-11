7b5ca43323e336f936e073fa49cba9ad5994b985
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-03-11 13:08:58 +0530

    Add initial Files

---
+++ script.js
@@ -0,0 +1,12 @@
+let specialBtn = document.querySelector("#special-btn");
+let display = document.querySelector(".input-box input");
+let minus = "-";
+
+specialBtn.addEventListener("click", () => {
+  specialBtn.classList.toggle("minus");
+  if (specialBtn.classList.contains("minus")) {
+    display.value = `${minus}${display.value}`;
+  } else {
+    display.value = display.value.toString().slice(1);
+  }
+});
