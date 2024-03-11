9c5f146f53779dd0bf681da608d0838db6c5a4b1
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-03-11 14:35:39 +0530

    add final changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let specialBtn = document.querySelector("#special-btn");
 let display = document.querySelector(".input-box input");
 let minus = "-";
@@ -10,3 +11,4 @@ specialBtn.addEventListener("click", () => {
     display.value = display.value.toString().slice(1);
   }
 });
+*/
